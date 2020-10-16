cba6d27de444a007fbe726053795fcfd0543b1a3
MarcChayer <marcci@mac.com>
2020-10-16 16:31:33 +0200

    insert dabasefile on database

--- back/data/create_database.sql
+++ back/data/create_database.sql
@@ -15,8 +15,8 @@ CREATE TABLE "user" (
     "imc" INTEGER NOT NULL,
     "height" INTEGER NOT NULL,
     "password" TEXT NOT NULL,
-    "is_admin" BOOLEAN NOT NULL,
-)
+    "is_admin" BOOLEAN NOT NULL
+);
 
 --
 -- Contenu de la table 'user'
@@ -35,10 +35,8 @@ CREATE TABLE "article" (
     "id" SERIAL PRIMARY KEY,
     "title" TEXT NOT NULL,
     "content" TEXT NOT NULL,
-    "media" TEXT NOT NULL,
-    "excerpt" TEXT NOT NULL,
-    "label" TEXT NOT NULL,
-)
+    "media" TEXT NOT NULL
+);
 
 --
 -- Contenu de la table 'article'
@@ -50,7 +48,7 @@ Doloremque perferendis sed magni doloribus autem ratione iste, deserunt itaque e
 Voluptatem commodi esse nihil quae ullam, non corrupti sint beatae, deleniti omnis, adipisci explicabo saepe. Cupiditate, fugiat voluptatum! Nostrum cumque blanditiis sapiente alias non ratione minus natus odio architecto sunt.
 Necessitatibus ab architecto placeat molestias consequuntur obcaecati aperiam maiores ipsam animi voluptatibus veniam dolor, vitae quos explicabo quaerat laudantium similique, quam culpa natus exercitationem neque ex modi ipsa? Deserunt, eius?
 Temporibus, voluptate? Optio ex, magnam ipsam quo rerum dolores maiores aliquam vel, aut accusamus commodi distinctio aperiam quibusdam ad accusantium excepturi dolore at, sapiente ab. Totam beatae sed error eaque.', 'https://cdn.pixabay.com/photo/2017/04/22/10/15/sport-2250970_960_720.jpg'),
-(2, 'c\'est la folie', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur obcaecati delectus nemo architecto ex, ullam vero non quia tenetur iure voluptates modi facilis quas incidunt numquam temporibus voluptatem corporis provident.
+(2, 'la folie du sport', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur obcaecati delectus nemo architecto ex, ullam vero non quia tenetur iure voluptates modi facilis quas incidunt numquam temporibus voluptatem corporis provident.
 Doloremque perferendis sed magni doloribus autem ratione iste, deserunt itaque ex. Repudiandae et quibusdam commodi mollitia consequatur culpa cumque consectetur, maiores exercitationem sint esse ex placeat dignissimos delectus rem magnam.
 Voluptatem commodi esse nihil quae ullam, non corrupti sint beatae, deleniti omnis, adipisci explicabo saepe. Cupiditate, fugiat voluptatum! Nostrum cumque blanditiis sapiente alias non ratione minus natus odio architecto sunt.
 Necessitatibus ab architecto placeat molestias consequuntur obcaecati aperiam maiores ipsam animi voluptatibus veniam dolor, vitae quos explicabo quaerat laudantium similique, quam culpa natus exercitationem neque ex modi ipsa? Deserunt, eius?
@@ -60,12 +58,12 @@ Doloremque perferendis sed magni doloribus autem ratione iste, deserunt itaque e
 Voluptatem commodi esse nihil quae ullam, non corrupti sint beatae, deleniti omnis, adipisci explicabo saepe. Cupiditate, fugiat voluptatum! Nostrum cumque blanditiis sapiente alias non ratione minus natus odio architecto sunt.
 Necessitatibus ab architecto placeat molestias consequuntur obcaecati aperiam maiores ipsam animi voluptatibus veniam dolor, vitae quos explicabo quaerat laudantium similique, quam culpa natus exercitationem neque ex modi ipsa? Deserunt, eius?
 Temporibus, voluptate? Optio ex, magnam ipsam quo rerum dolores maiores aliquam vel, aut accusamus commodi distinctio aperiam quibusdam ad accusantium excepturi dolore at, sapiente ab. Totam beatae sed error eaque.', 'https://cdn.pixabay.com/photo/2015/03/01/22/27/relay-race-655353_960_720.jpg'),
-(4, 'la bouffe c\'est la vie', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur obcaecati delectus nemo architecto ex, ullam vero non quia tenetur iure voluptates modi facilis quas incidunt numquam temporibus voluptatem corporis provident.
+(4, 'la bouffe : la vie', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur obcaecati delectus nemo architecto ex, ullam vero non quia tenetur iure voluptates modi facilis quas incidunt numquam temporibus voluptatem corporis provident.
 Doloremque perferendis sed magni doloribus autem ratione iste, deserunt itaque ex. Repudiandae et quibusdam commodi mollitia consequatur culpa cumque consectetur, maiores exercitationem sint esse ex placeat dignissimos delectus rem magnam.
 Voluptatem commodi esse nihil quae ullam, non corrupti sint beatae, deleniti omnis, adipisci explicabo saepe. Cupiditate, fugiat voluptatum! Nostrum cumque blanditiis sapiente alias non ratione minus natus odio architecto sunt.
 Necessitatibus ab architecto placeat molestias consequuntur obcaecati aperiam maiores ipsam animi voluptatibus veniam dolor, vitae quos explicabo quaerat laudantium similique, quam culpa natus exercitationem neque ex modi ipsa? Deserunt, eius?
 Temporibus, voluptate? Optio ex, magnam ipsam quo rerum dolores maiores aliquam vel, aut accusamus commodi distinctio aperiam quibusdam ad accusantium excepturi dolore at, sapiente ab. Totam beatae sed error eaque.', 'https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032_960_720.jpg'),
-(5, 'Manger, c\'est la base', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur obcaecati delectus nemo architecto ex, ullam vero non quia tenetur iure voluptates modi facilis quas incidunt numquam temporibus voluptatem corporis provident.
+(5, 'Manger : la base', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur obcaecati delectus nemo architecto ex, ullam vero non quia tenetur iure voluptates modi facilis quas incidunt numquam temporibus voluptatem corporis provident.
 Doloremque perferendis sed magni doloribus autem ratione iste, deserunt itaque ex. Repudiandae et quibusdam commodi mollitia consequatur culpa cumque consectetur, maiores exercitationem sint esse ex placeat dignissimos delectus rem magnam.
 Voluptatem commodi esse nihil quae ullam, non corrupti sint beatae, deleniti omnis, adipisci explicabo saepe. Cupiditate, fugiat voluptatum! Nostrum cumque blanditiis sapiente alias non ratione minus natus odio architecto sunt.
 Necessitatibus ab architecto placeat molestias consequuntur obcaecati aperiam maiores ipsam animi voluptatibus veniam dolor, vitae quos explicabo quaerat laudantium similique, quam culpa natus exercitationem neque ex modi ipsa? Deserunt, eius?
@@ -75,12 +73,12 @@ Doloremque perferendis sed magni doloribus autem ratione iste, deserunt itaque e
 Voluptatem commodi esse nihil quae ullam, non corrupti sint beatae, deleniti omnis, adipisci explicabo saepe. Cupiditate, fugiat voluptatum! Nostrum cumque blanditiis sapiente alias non ratione minus natus odio architecto sunt.
 Necessitatibus ab architecto placeat molestias consequuntur obcaecati aperiam maiores ipsam animi voluptatibus veniam dolor, vitae quos explicabo quaerat laudantium similique, quam culpa natus exercitationem neque ex modi ipsa? Deserunt, eius?
 Temporibus, voluptate? Optio ex, magnam ipsam quo rerum dolores maiores aliquam vel, aut accusamus commodi distinctio aperiam quibusdam ad accusantium excepturi dolore at, sapiente ab. Totam beatae sed error eaque.', 'https://cdn.pixabay.com/photo/2015/03/26/09/42/breakfast-690128_960_720.jpg'),
-(7, 'Le yoga aussi, c\'est la vie', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur obcaecati delectus nemo architecto ex, ullam vero non quia tenetur iure voluptates modi facilis quas incidunt numquam temporibus voluptatem corporis provident.
+(7, 'Le yoga : la vie', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur obcaecati delectus nemo architecto ex, ullam vero non quia tenetur iure voluptates modi facilis quas incidunt numquam temporibus voluptatem corporis provident.
 Doloremque perferendis sed magni doloribus autem ratione iste, deserunt itaque ex. Repudiandae et quibusdam commodi mollitia consequatur culpa cumque consectetur, maiores exercitationem sint esse ex placeat dignissimos delectus rem magnam.
 Voluptatem commodi esse nihil quae ullam, non corrupti sint beatae, deleniti omnis, adipisci explicabo saepe. Cupiditate, fugiat voluptatum! Nostrum cumque blanditiis sapiente alias non ratione minus natus odio architecto sunt.
 Necessitatibus ab architecto placeat molestias consequuntur obcaecati aperiam maiores ipsam animi voluptatibus veniam dolor, vitae quos explicabo quaerat laudantium similique, quam culpa natus exercitationem neque ex modi ipsa? Deserunt, eius?
 Temporibus, voluptate? Optio ex, magnam ipsam quo rerum dolores maiores aliquam vel, aut accusamus commodi distinctio aperiam quibusdam ad accusantium excepturi dolore at, sapiente ab. Totam beatae sed error eaque.', 'https://cdn.pixabay.com/photo/2017/03/26/21/54/yoga-2176668_960_720.jpg'),
-(8, 'lifestyle, ce qu\'il faut savoir', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur obcaecati delectus nemo architecto ex, ullam vero non quia tenetur iure voluptates modi facilis quas incidunt numquam temporibus voluptatem corporis provident.
+(8, 'lifestyle, il le faut savoir', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur obcaecati delectus nemo architecto ex, ullam vero non quia tenetur iure voluptates modi facilis quas incidunt numquam temporibus voluptatem corporis provident.
 Doloremque perferendis sed magni doloribus autem ratione iste, deserunt itaque ex. Repudiandae et quibusdam commodi mollitia consequatur culpa cumque consectetur, maiores exercitationem sint esse ex placeat dignissimos delectus rem magnam.
 Voluptatem commodi esse nihil quae ullam, non corrupti sint beatae, deleniti omnis, adipisci explicabo saepe. Cupiditate, fugiat voluptatum! Nostrum cumque blanditiis sapiente alias non ratione minus natus odio architecto sunt.
 Necessitatibus ab architecto placeat molestias consequuntur obcaecati aperiam maiores ipsam animi voluptatibus veniam dolor, vitae quos explicabo quaerat laudantium similique, quam culpa natus exercitationem neque ex modi ipsa? Deserunt, eius?
@@ -99,8 +97,8 @@ Temporibus, voluptate? Optio ex, magnam ipsam quo rerum dolores maiores aliquam 
 
 CREATE TABLE "favorite_article" (
     "user_id" INTEGER REFERENCES "user"("id") NOT NULL,
-    "article_id" INTEGER REFERENCES "article"("id") NOT NULL,
-)
+    "article_id" INTEGER REFERENCES "article"("id") NOT NULL
+);
 
 --
 -- Contenu de la table 'favorite_article'
@@ -120,8 +118,8 @@ CREATE TABLE "food" (
     "date" TIMESTAMP NOT NULL,
     "meal" TEXT NOT NULL,
     "water" INTEGER NOT NULL,
-    "emotion" TEXT NOT NULL,
-)
+    "emotion" TEXT NOT NULL
+);
 
 --
 -- Contenu de la table 'food'
@@ -140,15 +138,15 @@ CREATE TABLE "sleep" (
     "id" SERIAL PRIMARY KEY,
     "date" TIMESTAMP NOT NULL,
     "bedTime" TIME NOT NULL,
-    "wakeUpTime" TIME NOT NULL,
-)
+    "wakeUpTime" TIME NOT NULL
+);
 
 --
 -- Contenu de la table 'sleep'
 --
 
 INSERT INTO "sleep" ("id", "date", "bedTime", "wakeUpTime") VALUES
-(1,'15-10-20','12-00', '07-00');
+(1,'15-10-20','12:00', '07:00');
 
 -- --------------------------------------------------------
 
@@ -162,8 +160,8 @@ CREATE TABLE "sport" (
     "workout" TEXT NOT NULL,
     "duration" INTEGER NOT NULL,
     "intensity" TEXT NOT NULL,
-    "emotion" TEXT NOT NULL,
-)
+    "emotion" TEXT NOT NULL
+);
 
 --
 -- Contenu de la table 'sport'
@@ -180,8 +178,9 @@ INSERT INTO "sport" ("id", "date", "workout", "duration", "intensity", "emotion"
 
 CREATE TABLE "task" (
     "id" SERIAL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-)
+    "title" TEXT NOT NULL
+);
+
 
 --
 -- Contenu de la table 'task'
@@ -190,4 +189,5 @@ CREATE TABLE "task" (
 INSERT INTO "task" ("id", "title") VALUES
 (1,'reussir ce putain de projet');
 
--- --------------------------------------------------------
+
+COMMIT;
