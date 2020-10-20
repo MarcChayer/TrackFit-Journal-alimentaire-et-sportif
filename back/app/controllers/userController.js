0d31c5c29315c18cacb3b7c7cad96e3a4b9b0612
MarcChayer <marcci@mac.com>
2020-10-20 15:03:45 +0200

    bcrypt installation for signup ok

--- back/app/controllers/userController.js
+++ back/app/controllers/userController.js
@@ -1,13 +1,31 @@
+const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
 
 const userController = {
     signupAction: async (req, res) => {
         try {
-            console.log('user', req.body);
             const { lastName, firstName, email, password } = req.body;
+            // on créé une const contenant un array d'erreur
             const bodyError=[];
-            // On verifie que l'user n'est pas en BDD
+            // si l'utilisateur ne rempli les input, il reçoit une alert
+            if (lastName.trim() === '') {
+				bodyError.push('Merci de saisir votre nom');
+            }
+            if (firstName.trim() === '') {
+				bodyError.push('Merci de saisir votre prénom');
+			}
+            if (email.trim() === '') {
+				bodyError.push('Merci de saisir une adresse email');
+			}
+			if (password.trim() === '') {
+				bodyError.push('Merci de saisir un mot de passe');
+            }
+            // pas sur pour le "req.body.passwordConfirm" ????????
+			if (password !== password) {
+				bodyError.push('Les 2 mots de passe sont différents');
+			}
+			// On vérifie que l'utilisateur est présent en BDD
             const user = await User.findOne({ where: { email: email } });
             if (user) {
                 bodyError.push('Cet utilisateur existe déjà');
@@ -15,20 +33,29 @@ const userController = {
             if (bodyError.length) {
                 res.status(400).json(bodyError);
             } else {
+                // on stock le password crypté
+                const passwordEncrypted = bcrypt.hashSync(password, 10);
+
                 let newUser = User.build({
-                    lastName,
-                    firstName,
-                    email,
-                    password
+                    lastName: req.body.lastName,
+                    firstName: req.body.firstName,
+                    email: req.body.email,
+                    password: passwordEncrypted,
                 });
                 await newUser.save();
                 res.status(200).json(newUser);
+                // on redirige ensuite sur la page login
+                // res.redirect('/login');
             }
         } catch (error) {
             console.log(error);
             res.status(500).json(error.toString());
         }
     },
+
+	renderSignup: (req, res) => {
+		res.render('signup');
+	},
 }
 
 module.exports = userController;
