bff704314632c1782592f02ebbdc1b4c8dd29fd0
MatthieuDEP <matthieu.depret.md@gmail.com>
2020-10-22 09:46:32 +0200

    corrections ESLint

--- front/src/components/Login/index.js
+++ front/src/components/Login/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable import/no-unresolved */
 import React from 'react';
 import logo from 'src/assets/Logo_TrackFit_icone.svg';
 import './style.scss';
@@ -5,26 +7,21 @@ import './style.scss';
 const Login = () => (
   <div className="login">
     <img className="logotrackfit" src={logo} alt="trackFit" />
-    
     <h1>S'identifier</h1>
 
     <form className="formlog">
       <label>E-mail:</label>
-     <input className="log" type="text" placeholder="E-mail" />
-      
+      <input className="log" type="text" placeholder="E-mail" />
       <label>Mot de passe:</label>
       <input className="log" type="text" placeholder="Mot de passe" />
-      
       <label>
-        <input type="checkbox"/>
+        <input type="checkbox" />
         Rester connecté
       </label>
-
-      <button>S'identifier</button>
+      <button type="submit">S'identifier</button>
     </form>
 
-    <p>Vous ne possèdez pas de compte?<a className="id" href = "#" > Inscrivez-vous</a></p>
-    
+    <p>Vous ne possèdez pas de compte?<a className="id" href="#"> Inscrivez-vous</a></p>
   </div>
 );
 
