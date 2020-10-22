1a9e2bf31ea08e9b1d1f36b830f6067092d4ad53
MatthieuDEP <matthieu.depret.md@gmail.com>
2020-10-22 09:40:59 +0200

    corrections ESLint

--- front/src/components/CreateAccount/index.js
+++ front/src/components/CreateAccount/index.js
@@ -1,39 +1,31 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable import/no-unresolved */
 import React from 'react';
 import logo from 'src/assets/Logo_TrackFit_icone.svg';
 import './style.scss';
 
-
 const CreateAccount = () => (
-<div className="createaccount">
+  <div className="createaccount">
 
-  <img className="logotrackfit" src={logo} alt="trackFit" />
-  <h1>Créer un compte</h1>
+    <img className="logotrackfit" src={logo} alt="trackFit" />
+    <h1>Créer un compte</h1>
 
-  <form className="createform">
-  
-  <label>Votre nom :</label>
-    <input className="create" type="text" placeholder="Nom" />
-  
-  <label>Votre prénom :</label>
-    <input className="create" type="text" placeholder="prénom" />
+    <form className="createform">
+      <label>Votre nom :</label>
+      <input className="create" type="text" placeholder="Nom" />
+      <label>Votre prénom :</label>
+      <input className="create" type="text" placeholder="prénom" />
+      <label>E-mail:</label>
+      <input className="create" type="text" placeholder="E-mail" />
+      <label>Mot de passe:</label>
+      <input className="create" type="text" placeholder="Mot de passe" />
+      <label>Mot de passe à nouveau</label>
+      <input className="create" type="text" placeholder="Mot de passe à nouveau" />
+      <button type="submit">Créer votre compte</button>
+    </form>
 
+    <p>Vous avez déjà compte?<a className="id" href="#"> Identifiez-vous</a></p>
 
-  <label>E-mail:</label>
-    <input className="create" type="text" placeholder="E-mail" />
-  
-  <label>Mot de passe:</label>
-    <input className="create"type="text" placeholder="Mot de passe" />
-  
-  <label>Mot de passe à nouveau</label>
-    <input className="create" type="text" placeholder="Mot de passe à nouveau" />
-
-  <button>Créer votre compte</button>
-  </form>
-
-  
-  <p>Vous avez déjà compte?<a className="id" href = "#" > Identifiez-vous</a></p>
-  
-  
-</div>
+  </div>
 );
 export default CreateAccount;
