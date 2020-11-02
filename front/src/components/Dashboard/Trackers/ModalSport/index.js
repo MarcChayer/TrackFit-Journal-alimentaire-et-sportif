f3166d5db5b19270e7ce8db4154c5a16dd795f95
paurtfolio <paurtfolio@outlook.fr>
2020-11-02 09:10:05 +0100

    input corrected

--- front/src/components/Dashboard/Trackers/ModalSport/index.js
+++ front/src/components/Dashboard/Trackers/ModalSport/index.js
@@ -1,26 +1,32 @@
-import React from "react";
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable react/button-has-type */
+import React from 'react';
 import Sport from 'src/assets/images/trackers/icone-sport.svg';
 
 import './modalsport.scss';
 
 const ModalSport = ({ onClick }) => (
 
-<div className="modal-sport">
-<img className="logo-sport" src={Sport} alt="" />
-<h1 className="mod-title">Activité :</h1>
-<div className="mod-cal">
-      <label className="mod-label">Dépense estimée :</label>
-      <p className="mod-para">1680 cal. consommée</p>
-</div>
-<form className="mod-form">
-      <label className="mod-label">Activité physique :</label>
-      <input className="mod-input-sport" type="text"  />
-      <label className="mod-label">Intensité :</label>
-      <input className="mod-input-sport" type="text"  />
-      <label className="mod-label">Durée de la séance :</label>
-      <input className="mod-input-sport" type="text"  />
-</form>
-    <button className="modal-button"onClick={ onClick }> Valider</button>
-</div>
+  <div className="modal-sport">
+    <img className="logo-sport" src={Sport} alt="" />
+    <h1 className="mod-title-sport">Activité physique</h1>
+    <div className="mod-cal-sport">
+      <label className="mod-label-sport">Dépense estimée :</label>
+      <p className="mod-para">x cal. consommée</p>
+    </div>
+    <form className="mod-form-sport">
+      <label className="mod-label-sport">Activité physique :</label>
+      <input className="mod-input-sport" type="text" />
+      <label className="mod-label-sport">Intensité :</label>
+      <select className="mod-input-sport" name="intensities" id="intensity-select">
+        <option value="1">Effort léger</option>
+        <option value="2">Effort modéré</option>
+        <option value="3">Effort intense</option>
+      </select>
+      <label className="mod-label-sport">Durée de la séance :</label>
+      <input className="mod-input-sport" type="time" />
+    </form>
+    <button className="modal-button-sport" onClick={onClick}> Valider</button>
+  </div>
 );
 export default ModalSport;
