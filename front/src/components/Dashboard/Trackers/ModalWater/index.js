606da8c3481f81fcbcf04edb5684038e76fabf36
paurtfolio <paurtfolio@outlook.fr>
2020-11-03 01:22:52 +0100

    lots of modification on CSS + Hamburger Menu created with my pal Alexis

--- front/src/components/Dashboard/Trackers/ModalWater/index.js
+++ front/src/components/Dashboard/Trackers/ModalWater/index.js
@@ -1,14 +1,16 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable react/button-has-type */
 import React from 'react';
-import sleep from 'src/assets/images/trackers/icone-water.svg';
+import water from 'src/assets/images/trackers/icone-water.svg';
+import illuwater from 'src/assets/images/illustrations/illustration-water.svg';
 
 import './modalwater.scss';
 
 const ModalWater = ({ onClick }) => (
 
   <div className="modal-water">
-    <img className="logo-water" src={sleep} alt="" />
+    <img className="logo-water" src={water} alt="" />
+    <img className="illu-water" src={illuwater} alt="" />
     <h1 className="mod-title-water">Consommation d'eau</h1>
     <form className="mod-form-water">
       <label className="mod-label-water">Consommation d'eau (en cl) :</label>
