b2f36c49639e5e45b3a78b07f041dab3f8bfae5f
MarcChayer <marcci@mac.com>
2020-11-05 21:27:39 +0100

    update updateProfil

--- back/app/controllers/userController.js
+++ back/app/controllers/userController.js
@@ -125,6 +125,7 @@ const userController = {
             if (!user) {
                 res.status(404).json({error: 'Cet utilisateur n\'existe pas'});
             } else {
+                
                 await user.update(req.body);
                 res.status(200).json(user);
             }
