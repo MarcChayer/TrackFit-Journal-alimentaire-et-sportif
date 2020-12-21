5522fd930ed88f1310ebfc6221c171d1ca9846cd
paurtfolio <paurtfolio@outlook.fr>
2020-12-21 13:32:03 +0100

    correction for CSS

--- front/src/components/Burger/index.js
+++ front/src/components/Burger/index.js
@@ -2,14 +2,13 @@
 /* eslint-disable react/button-has-type */
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import hamburger from 'src/assets/images/icone-nav-burger.svg';
 import './burger.scss';
 
 const ModalBurger = ({ onClick, isLogged }) => (
 
   <div className="modal-burger-content">
     <ul className="navigation-list-mobile">
-      <li><NavLink to="/" className="navigation-link-mobile" activeClassName="navigation-link--active" onClick={onClick}>Accueil</NavLink></li>
+      <li><NavLink exact to="/" className="navigation-link-mobile" activeClassName="navigation-link--active" onClick={onClick}>Accueil</NavLink></li>
       <li><NavLink to="/articles/alimentation" className="navigation-link-mobile" activeClassName="navigation-link--active" onClick={onClick}>Alimentation</NavLink></li>
       <li><NavLink to="/articles/sport" className="navigation-link-mobile" activeClassName="navigation-link--active" onClick={onClick}>Sport</NavLink></li>
       <li><NavLink to="/articles/bienetre" className="navigation-link-mobile" activeClassName="navigation-link--active" onClick={onClick}>Bien-être</NavLink></li>
@@ -19,14 +18,6 @@ const ModalBurger = ({ onClick, isLogged }) => (
     <button className="navigation--button--close" onClick={onClick}>
       <p>X</p>
     </button>
-
-    {/* ou image */}
-    {/* <img
-      onClick={onClick}
-      src={hamburger}
-      alt="menu"
-      className="navigation--img--close"
-    /> */}
   </div>
 );
 export default ModalBurger;
